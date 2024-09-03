02fcf963517edd5b5612dea987baf07436f52910
melvinsjostrand <89245348+melvinsjostrand@users.noreply.github.com>
2024-09-03 19:29:34 +0200

    started project
    downloaded what needed
    for exampel : chakra, npm i

---
+++ game-hub/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Button colorScheme="blue">Button</Button>
+    </>
+  );
+}
+
+export default App;
