44546061a5e36f20f931311105cc3e0f3f8deb26
melvinsjostrand <89245348+melvinsjostrand@users.noreply.github.com>
2024-09-04 17:42:27 +0200

    cleanup gamecards

--- game-hub/src/components/GameDisplay/GameCard.tsx
+++ game-hub/src/components/GameDisplay/GameCard.tsx
@@ -9,16 +9,16 @@ interface Props {
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card height='100%'>
+    <Card height="100%">
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent={"space-between"}>
+        <HStack justifyContent={"space-between"} marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
