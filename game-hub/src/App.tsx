2fb4cab37544c27db083de7755f2d8a055d3da9c
melvinsjostrand <89245348+melvinsjostrand@users.noreply.github.com>
2024-09-04 17:16:15 +0200

    sort games + handle games with no image

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -12,6 +12,7 @@ import SortSelector from "./components/SortSelector";
 export interface GameQuery {
   genre: Genre | null;
   platform : Platform | null;
+  sortOrder: string;
 }
 
 
@@ -48,7 +49,7 @@ function App() {
               setGameQuery({ ...gameQuery, platform })
             }
           />
-          <SortSelector />
+          <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder})}/>
         </HStack>
         <GameGrid gameQuery={gameQuery}></GameGrid>
       </GridItem>
